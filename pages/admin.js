9f34ca2b91f0ee3225d78103472b9df239ac0fc8
peter.nortey <peter.nortey@azubiafrica.org>
2021-07-31 17:14:39 +0000

    uncommented out useUser

--- pages/admin.js
+++ pages/admin.js
@@ -1,17 +1,17 @@
-import useUser from '../lib/useUser'
-import Layout from '../components/Layout'
+// import useUser from '../lib/useUser'
+// import Layout from '../components/Layout'
 
-const Admin = () => {
-    //Fetch the user client side
-    const {user} = useUser({redirectTo: '/login'})
+// const Admin = () => {
+//     //Fetch the user client side
+//     const {user} = useUser({redirectTo: '/login'})
 
-    //Server-render loading state
-    if (!user || !user.isLoggedIn == false) {
-        return <Layout><p>Loading...</p></Layout>
-    }
+//     //Server-render loading state
+//     if (!user || !user.isLoggedIn == false) {
+//         return <Layout><p>Loading...</p></Layout>
+//     }
 
-    //Once the user is logged in, render the admin page
-    return <Layout><p>Admin Page</p></Layout>
-}
+//     //Once the user is logged in, render the admin page
+//     return <Layout><p>Admin Page</p></Layout>
+// }
 
-export default Admin;
+// export default Admin;
