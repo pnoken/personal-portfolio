e956f055ea70f7279e2500cc63ff55eb9e005900
peter.nortey <peter.nortey@azubiafrica.org>
2021-08-17 08:57:22 +0000

    uncommented out blog slug

--- pages/blog/[slug]/index.js
+++ pages/blog/[slug]/index.js
@@ -1,39 +1,39 @@
-const Blog = () => {
-  return (
-    <>
-      <h1>My Blog</h1>
-    </>
-  );
+// const Blog = () => {
+//   return (
+//     <>
+//       <h1>My Blog</h1>
+//     </>
+//   );
 
-  export async function getStaticPaths() {
-    return {
-      fallback: false,
-      paths: [
-        {
-          params: {
-            slug: "first-blog",
-          },
-        },
-        {
-          params: {
-            slug: "first-blog",
-          },
-        },
-      ],
-    };
-  }
+//   export async function getStaticPaths() {
+//     return {
+//       fallback: false,
+//       paths: [
+//         {
+//           params: {
+//             slug: "first-blog",
+//           },
+//         },
+//         {
+//           params: {
+//             slug: "first-blog",
+//           },
+//         },
+//       ],
+//     };
+//   }
 
-  export async function getStaticProps(context) {
-    //Fetch Data from API
-    const blog_id = context.params.slug;
-    const BLOG_DATA = await fetch("/api/blog/");
-    return {
-      props: {
-        blogData: BLOG_DATA,
-      },
-      revalidate: 3600,
-    };
-  }
-};
+//   export async function getStaticProps(context) {
+//     //Fetch Data from API
+//     const blog_id = context.params.slug;
+//     const BLOG_DATA = await fetch("/api/blog/");
+//     return {
+//       props: {
+//         blogData: BLOG_DATA,
+//       },
+//       revalidate: 3600,
+//     };
+//   }
+// };
 
-export default Blog;
+// export default Blog;
