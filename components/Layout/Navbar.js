ebffbb9fb7834609f2eea812a1e1a302031b141d
pnoken <ecexpay@gmail.com>
2021-10-02 07:36:09 +0000

    set dark and light theme in local storage

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-export default function Navbar({ themeToggler }) {
+export default function Navbar({ themeToggler, checked }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
@@ -12,80 +12,16 @@ export default function Navbar({ themeToggler }) {
         </a>
         <span className="p-2">AM</span>
         <label className="switch">
-          <input type="checkbox" onClick={themeToggler} />
+          {checked ? (
+            <input type="checkbox" onChange={themeToggler} checked />
+          ) : (
+            <input type="checkbox" onChange={themeToggler} />
+          )}
           <span className="slider round"></span>
         </label>
         <span className="p-2">PM</span>
-        {/* <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarScroll"
-          aria-controls="navbarScroll"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button> */}
-        <div className="collapse navbar-collapse" id="navbarScroll">
-          {/* <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"> */}
-          {/* <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                About Me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Featured Projects
-              </a>
-            </li> */}
-          {/* <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarScrollingDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Link
-              </a>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="navbarScrollingDropdown"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li> */}
-          {/* <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabIndex="-1"
-                aria-disabled="true"
-              >
-                News
-              </a>
-            </li> */}
-          {/* </ul> */}
-        </div>
+
+        <div className="collapse navbar-collapse" id="navbarScroll"></div>
       </div>
     </nav>
   );
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,17 +2,40 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import styles from "../darktheme.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function MainLayout({ title, children }) {
   const [theme, setTheme] = useState("light");
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
+  const [checked, setChecked] = useState(false);
+  const themeToggler = (e) => {
+    let checked = e.target.checked;
+    console.log("checked", checked);
+    checked
+      ? (setTheme("dark"), localStorage.setItem("theme", "dark"))
+      : (setTheme("light"),
+        localStorage.setItem("theme", "light"),
+        setChecked(false));
   };
+
+  useEffect(() => {
+    if (localStorage.theme === "dark") {
+      setTheme("dark");
+      setChecked(true);
+    }
+  }, []);
+
+  // Router.onRouteChangeStart = (url) => {
+  //   NProgress.start();
+  // };
+
+  // Router.onRouteChangeComplete = () => NProgress.done();
+
+  // Router.onRouteChangeError = () => NProgress.done();
+
   return (
     <div className={theme === "light" ? "" : styles.body}>
       <Header title={title || "Peter Nortey"} />
-      <Navbar themeToggler={themeToggler} />
+      <Navbar themeToggler={themeToggler} checked={checked} />
       <div>{children}</div>
       <Footer />
     </div>
