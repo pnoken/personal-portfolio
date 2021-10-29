9d58f2d2515b24da0cf731368f4254a5623c1a70
pnoken <ecexpay@gmail.com>
2021-10-29 09:03:27 +0000

    added lazy loading

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import MainLayout from "../components/Layout";
-import Banner from "../components/Section/Banner";
+import dynamic from "next/dynamic";
+//import Banner from "../components/Section/Banner";
 import MidSection from "../components/Section/MidSection";
 import Newsletter from "../components/Section/Newsletter";
+
+const Banner = dynamic(() => import("../components/Section/Banner"), {
+  loading: () => (
+    <div class="spinner-border m-5" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  ),
+});
 
 export default function Home() {
   return (
