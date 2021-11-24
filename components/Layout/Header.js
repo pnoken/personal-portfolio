642500ee0d1f7716365b806d57d8f04ef64fcdf2
pnoken <ecexpay@gmail.com>
2021-11-24 12:33:28 +0000

    added meta info to header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -7,6 +7,15 @@ export default function Header({ title }) {
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="I am Peter Nortey, A JavaScript Developer with over two years professional experience as a frontend developer."
+      />
+      <meta
+        name="keywords"
+        content="HTML, CSS, JavaScript, Software Engineer, Peter, React, NodeJS, Ghana, Software Developer in Accra"
+      />
+      <meta name="author" content="Peter Nortey" />
       <link
         href="https://fonts.googleapis.com/css2?family=Krona+One&display=optional"
         rel="stylesheet"
