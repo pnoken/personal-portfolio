8d9e0573996bc555042c8bdcb4bfe30d1fc030f3
pnoken <ecexpay@gmail.com>
2021-11-24 12:41:14 +0000

    added image for meta

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -8,13 +8,14 @@ export default function Header({ title }) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
-        name="description"
+        property="og:description"
         content="I am Peter Nortey, A JavaScript Developer with over two years professional experience as a frontend developer."
       />
       <meta
         name="keywords"
         content="HTML, CSS, JavaScript, Software Engineer, Peter, React, NodeJS, Ghana, Software Developer in Accra"
       />
+      <meta property="og:image" content="/images/mtipet.jpg" />
       <meta name="author" content="Peter Nortey" />
       <link
         href="https://fonts.googleapis.com/css2?family=Krona+One&display=optional"
